a123fd688d50bde544202571ef9cf52122c451c3
Kyohei Hamada <kyohei.hamada@gmail.com>
2013-03-31 15:00:20 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnsenhackRails::Application.config.secret_key_base = 'e7d2ac90f00a98408b8082addada6f521d4d2d85ef622461c30ab96ce7e5f5f609a25ca9d537122b27b27d570cd8ff6577b8fc7dce1199f0963485e958900577'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OnsenhackRails::Application.config.session_store :encrypted_cookie_store, key: '_onsenhack-rails_session'
